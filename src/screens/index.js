a64ff3c32e5bdfc1c0a01e8b20372e1431666794
Burak Seçer <brkscr@hotmail.com.tr>
2023-02-05 17:42:36 +0300

    clear too

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,8 +1,3 @@
-export {default as StartScreen} from './StartScreen'
-export {default as RegisterScreen} from './RegisterScreen'
-export {default as PageOne} from './PageOne'
-export {default as PageTwo} from './PageTwo'
-export {default as SplashScreen} from './SplashScreen'
 export {default as SignInScreen} from './SignInScreen'
 export {default as OtpScreen} from './OtpScreen'
 export {default as EmptyScreen} from './EmptyScreen'
